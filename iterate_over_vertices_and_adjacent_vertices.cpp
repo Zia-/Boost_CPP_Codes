8b535f9dfb77ff4435317a7b4005b0815c92b380
Mohammed Zia <mohammed.zia33@gmail.com>
2015-08-10 09:49:58 +0300

    added iterate_over_vertices_and_adjacent_vertices.cpp

---
+++ iterate_over_vertices_and_adjacent_vertices.cpp
@@ -0,0 +1,50 @@
+//============================================================================
+// Name        : boost_test.cpp
+// Author      : Zia
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <boost/graph/adjacency_list.hpp>
+
+using namespace boost;
+using namespace std;
+
+/*The purpose of typedef is to form complex types from more-basic machine types[1]
+and assign simpler names to such combinations.*/
+typedef boost::adjacency_list<listS, vecS, undirectedS> mygraph;
+
+int main(){
+	mygraph g;
+	add_edge (0,1,g);
+	add_edge (0,3,g);
+	add_edge (1,2,g);
+	add_edge (2,3,g);
+
+	//Here we are creating pointers which will refer to the indexes of the graph
+	mygraph::vertex_iterator vertexIt, vertexEnd;
+	mygraph::adjacency_iterator neighbourIt, neighbourEnd;
+
+	/*Giving vertexIt and vertexEnd pointers values.
+	Following vertices f() is a boost f()*/
+	tie(vertexIt, vertexEnd) = vertices(g);
+
+	//The following for loop will go from 0-3 since *vertexEnd = 4
+	for (; vertexIt != vertexEnd; ++vertexIt)
+	{
+		cout << *vertexIt << " vertex is connected with ";
+
+		//Following adjacent_vertices f() is a boost f()
+		tie(neighbourIt, neighbourEnd) = adjacent_vertices(*vertexIt, g);
+
+		for(; neighbourIt != neighbourEnd; ++neighbourIt)
+			cout << *neighbourIt << " ";
+		cout << "vertices \n";
+	}
+}
+
+
+
+
